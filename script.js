96c7b0a5e8e393642082a010ff7a11c576d98c5f
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-17 17:54:15 +0100

    esercizio completato con ciclo for

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const listaSpesa = ['latte', 'uova', 'farina', 'cetrioli', 'burro', 'scottex']
+
+const numeroElementiLista = listaSpesa.length
+console.log(numeroElementiLista)
+
+for (let i = 0; i < numeroElementiLista; i++){
+    console.log(listaSpesa[i])
+}
