fc039ad92d75685100341f4b792cf5f526c2d448
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-17 18:09:08 +0100

    risoluzione con ciclo while

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const listaSpesa = ['latte', 'uova', 'farina', 'cetrioli', 'burro', 'scottex']
 
 const numeroElementiLista = listaSpesa.length
-console.log(numeroElementiLista)
 
 /* for (let i = 0; i < numeroElementiLista; i++){
     console.log(listaSpesa[i])
 } */
+
+let elementoDaStampare = 0
+
+while (elementoDaStampare < listaSpesa.length){
+    console.log(listaSpesa[elementoDaStampare])
+    elementoDaStampare += 1
+}
