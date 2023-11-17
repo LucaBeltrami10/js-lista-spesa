1e252c241eb682a778bd80312d31779583013216
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-17 17:54:37 +0100

    commentato risoluzione ciclo for

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const listaSpesa = ['latte', 'uova', 'farina', 'cetrioli', 'burro', 'scottex']
 const numeroElementiLista = listaSpesa.length
 console.log(numeroElementiLista)
 
-for (let i = 0; i < numeroElementiLista; i++){
+/* for (let i = 0; i < numeroElementiLista; i++){
     console.log(listaSpesa[i])
-}
+} */
